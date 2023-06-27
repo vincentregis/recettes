135131db9fc741afdb7b430cde42d4876ec03bab
Vincent Regis <vincent.regis1@gmail.com>
2023-06-26 21:14:18 -0400

    robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://mellifluous-kringle-4809b2.netlify.app/',
-  integrations: [sitemap()]
+  integrations: [sitemap(), robotsTxt()]
 });
